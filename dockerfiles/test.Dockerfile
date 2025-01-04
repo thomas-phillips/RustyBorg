03e21acbc79528fed2e0ca7feb8122e24d83369d
Thomas Phillips <tphillips2020@gmail.com>
2025-01-04 23:07:55 +1030

    Added tests for verifying ssh connection and updated docker compose file for running tests
    I just realised that the SSH crate uses `unsafe` so this is something I
    want to look into in the future...

--- dockerfiles/test.Dockerfile
+++ dockerfiles/test.Dockerfile
@@ -1,24 +1,15 @@
-# Use an Ubuntu base image
-FROM ubuntu:22.04
+FROM rust:latest
 
-# Update package list and install OpenSSH Server
-RUN apt-get update && apt-get install -y openssh-server
 
-# Create the directory for SSH server and set permissions
-RUN mkdir /var/run/sshd
+WORKDIR /usr/src/rusty_borg
 
-# Set a password for the root user (Replace 'rootpassword' with a strong password)
-RUN echo 'root:rootpassword' | chpasswd
+RUN apt-get update && apt-get install -y \
+  build-essential borgbackup libssh-dev openssh-client
 
-# Enable root login via SSH
-RUN sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+COPY Cargo.lock Cargo.lock
+COPY Cargo.toml Cargo.toml
+COPY src/ src/
 
-# Disable PAM authentication (optional)
-RUN sed -i 's@session    required     pam_loginuid.so@session optional     pam_loginuid.so@g' /etc/pam.d/sshd
+RUN cargo build
 
-# Expose the SSH port
-EXPOSE 22
-
-# Start the SSH server
-CMD ["/usr/sbin/sshd", "-D"]
-
+CMD [ "cargo", "test" ]
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -26,7 +26,7 @@ pub enum LogLevel {
 // If an error occurs then the error will be propagated to the caller.
 pub fn verify_connection(verify_args: VerifyArgs) -> Result<(), String> {
     let port_usize = usize::try_from(verify_args.port).unwrap();
-    let mut session = Session::new().unwrap();
+    let mut session = Session::new().map_err(|_| format!("Error creating session object"))?;
 
     session
         .set_username(verify_args.user.as_str())
@@ -40,6 +40,10 @@ pub fn verify_connection(verify_args: VerifyArgs) -> Result<(), String> {
     session
         .connect()
         .map_err(|e| format!("Error connecting to host - {}", e))?;
+    session
+        .userauth_publickey_auto(Some("Test"))
+        .map_err(|e| format!("ERROR - {}", e))?;
+
     Ok(())
 }
 
@@ -63,8 +67,9 @@ pub fn log_print(message: &str, level: LogLevel) {
     }
 }
 
+#[allow(dead_code)]
 pub fn get_temp_directory() -> String {
-    tempfile::TempDir::new()
+    tempfile::tempdir()
         .expect("Failed to create a temporary directory")
         .path()
         .to_str()
@@ -72,10 +77,90 @@ pub fn get_temp_directory() -> String {
         .to_owned()
 }
 
+#[allow(dead_code)]
 pub fn get_random_string(length: usize) -> String {
     rand::thread_rng()
         .sample_iter(&Alphanumeric)
         .take(length)
         .map(char::from)
         .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const USER_KEY: &str = "TARGET_USER";
+    const HOST_KEY: &str = "TARGET_HOST";
+    const PORT_KEY: &str = "TARGET_PORT";
+
+    fn env_setup_verify_args(user_key: &str, host_key: &str, port_key: &str) -> VerifyArgs {
+        let user = match env::var(user_key) {
+            Ok(n) => n,
+            Err(_) => panic!("Can't get user environment variable!"),
+        };
+        let host = match env::var(host_key) {
+            Ok(n) => n,
+            Err(_) => panic!("Can't get host environment variable!"),
+        };
+        let port = match env::var(port_key) {
+            Ok(n) => n,
+            Err(_) => panic!("Can't get port environment variable!"),
+        };
+
+        VerifyArgs {
+            user: user.to_owned(),
+            host: host.to_owned(),
+            port: port.parse().unwrap(),
+        }
+    }
+
+    #[test]
+    fn test_verify_connection_pass() {
+        let verify_args = env_setup_verify_args(USER_KEY, HOST_KEY, PORT_KEY);
+        match verify_connection(verify_args) {
+            Ok(_) => assert!(true),
+            Err(e) => {
+                println!("{}", e);
+                assert!(false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_verify_connection_user_fail() {
+        let mut verify_args = env_setup_verify_args(USER_KEY, HOST_KEY, PORT_KEY);
+        verify_args.user = get_random_string(6).to_owned();
+        match verify_connection(verify_args) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true),
+        }
+    }
+
+    #[test]
+    fn test_verify_connection_host_fail() {
+        let mut verify_args = env_setup_verify_args(USER_KEY, HOST_KEY, PORT_KEY);
+        verify_args.host = "0.0.0.0.0.0.0.0".to_owned();
+        match verify_connection(verify_args) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true),
+        }
+    }
+
+    #[test]
+    fn test_verify_connection_port_fail() {
+        let mut verify_args = env_setup_verify_args(USER_KEY, HOST_KEY, PORT_KEY);
+        verify_args.port = 9999;
+        match verify_connection(verify_args) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true),
+        }
+    }
+
+    #[test]
+    fn test_get_random_string() {
+        for n in 1..101 {
+            assert_eq!(get_random_string(n).len(), n);
+        }
+    }
 }
